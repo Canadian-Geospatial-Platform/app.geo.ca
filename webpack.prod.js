d246fc8c874e961ed2fea90d189d1870138f4f4f
chris <chris@indiciumx.com>
2021-01-14 13:10:33 -0500

    210114 Changed start

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,17 +5,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const config = {
-    mode: 'development',
-    devServer: {
-    host: process.env.IP,
-    https: false,
-    disableHostCheck: true,
-    historyApiFallback: true,
-    contentBase: './dist',
-    overlay: true,
-    hot: true,
-    port: 8080,
-    },
+    mode: 'production'
 };
 
 module.exports = merge(common, config);
