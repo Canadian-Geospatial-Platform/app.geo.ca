a29c51968c8787eb15b470a24c62b057c1528ccf
Dennis <dennis.fu@canada.ca>
2021-01-27 16:48:25 -0500

    hash router

--- src/components/search/metadatapage.jsx
+++ src/components/search/metadatapage.jsx
@@ -45,7 +45,7 @@ const MetaDataPage = (props) => {
     };
   
     const handleKeyword = (keyword) => {
-      window.open("/search?keyword="+encodeURI(keyword.trim())+"&lang="+language+"&theme="+theme, "Search " + keyword.trim() );
+      window.open("/#/search?keyword="+encodeURI(keyword.trim())+"&lang="+language+"&theme="+theme, "Search " + keyword.trim() );
     }
   
     useEffect(() => {
