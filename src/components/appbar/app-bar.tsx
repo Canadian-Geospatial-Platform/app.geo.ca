4fa5e554e7910f4edddd803dd2a7d1df426e99af
Dennis <dennis.fu@canada.ca>
2021-02-09 15:57:09 -0500

    set filter icon/panel in app-bar, move filters to left cloumn in search page

--- src/components/appbar/app-bar.tsx
+++ src/components/appbar/app-bar.tsx
@@ -11,9 +11,11 @@ import { DomEvent } from 'leaflet';
 
 //import Layers from './buttons/layers';
 import Search from './buttons/search';
+import KeywordSearch from './buttons/keywordsearch';
+import Filter from './buttons/filters';
 import Account from './buttons/account';
 import Version from './buttons/version';
-import KeywordSearch from './buttons/keywordsearch';
+
 
 const drawerWidth = 200;
 
@@ -100,6 +102,7 @@ export function Appbar(props: AppBarProps): JSX.Element {
                     ))}*/}
                     {search && <Search key="geosearch" />}
                     {search && <KeywordSearch key="keywordsearch" />}
+                    <Filter key="filters" />
                 </List>
                 <Divider className={classes.spacer} />
                 {auth && <List><Account key="auth" /></List>}
