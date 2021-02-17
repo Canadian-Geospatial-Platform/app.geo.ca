624a1cb6f6e7230b3360eca3ac126edba0805ab0
David Deng <deng2018@yahoo.com>
2021-02-17 09:45:28 -0500

    Jira 202 extra: Add back to home landing page no matter what sub-page we are in when clicking home buton

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -38,7 +38,12 @@ export default function Header(): JSX.Element {
                     <div className="col-9 header-nav-col">
                         <nav className="header-nav">
                             <ul className="list-group flex-row justify-content-end align-items-center menu-list">
-                                <li className="list-group-item" onClick={()=>history.push('/')}>{t('nav.home')}</li>
+                                <li className="list-group-item" 
+                                    // onClick={()=>history.push('/#/')}
+                                    onClick={()=> window.location.reload(false)}
+                                    >                                       
+                                        {t('nav.home')}
+                                </li>
                                 <li className="list-group-item" onClick={() => changeLanguage(t('nav.language.key'))}>{t('nav.language.name')}</li>
                             </ul>
                         </nav>
