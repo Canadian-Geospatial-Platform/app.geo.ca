221d93a7d6c6eb781e0a1d0e786a97631095c382
Dennis <dennis.fu@canada.ca>
2021-02-24 17:44:56 -0500

    howto content

--- src/components/howto/howto-panel.tsx
+++ src/components/howto/howto-panel.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Typography } from '@material-ui/core';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import SearchIcon from '@material-ui/icons/ImageSearch';
@@ -7,26 +8,25 @@ import { useTranslation } from 'react-i18next';
 
 import PanelApp, {PanelProps} from '../appbar/panel';
 
-export default function HowtoPanel(props: PanelProps): JSX.Element {    
+export default function HowtoPanel(props: PanelProps): JSX.Element {
+    const { showing, closeFunction } = props;   
     const { t } = useTranslation();
     
     return (
         <PanelApp
             title="appbar.howto"
             icon={<HelpOutlineIcon />}
-            showing = {props.showing}
-            closeFunction = {props.closeFunction}
+            showing = {showing}
+            closeFunction = {closeFunction}
             content={
                 ((
                     <Typography variant="body2" color="textSecondary" component="div">                        
-                        <h4 className="text-center"> {<SearchIcon/>} {t('appbar.search')} </h4>
-                        <p>  {t('howto.geosearchdescription')} </p>                        
-                        
-                        <h4 className="text-center">  {<KeywordSearchIcon/>} {t('appbar.keywordsearch')}  </h4>
-                        <p>  {t('howto.keywordsearchdescription')} </p>                        
-                        
-                        <h4 className="text-center">  {<FilterIcon/>} {t('appbar.filters')} </h4>
-                        <p>  {t('howto.filtersdescription')} </p>                              
+                        <h4 className="text-center"> <SearchIcon/> {t('appbar.search')} </h4>
+                        <p>{t('howto.geosearchdescription')}</p>                        
+                        <h4 className="text-center"> <KeywordSearchIcon/> {t('appbar.keywordsearch')}  </h4>
+                        <p>{t('howto.keywordsearchdescription')}</p>                        
+                        <h4 className="text-center"> <FilterIcon/> {t('appbar.filters')} </h4>
+                        <p>{t('howto.filtersdescription')}</p>                              
                     </Typography>
                 ) as unknown) as Element
             }
