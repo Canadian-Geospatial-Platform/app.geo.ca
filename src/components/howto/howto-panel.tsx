641417e3fa71c4f5435fd095db625baefab87f02
David Deng <deng2018@yahoo.com>
2021-02-24 17:35:10 -0500

    Jira 289 Add How to Contents

--- src/components/howto/howto-panel.tsx
+++ src/components/howto/howto-panel.tsx
@@ -1,10 +1,14 @@
 import { Typography } from '@material-ui/core';
-//import SearchIcon from '@material-ui/icons/Search';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SearchIcon from '@material-ui/icons/ImageSearch';
+import KeywordSearchIcon from '@material-ui/icons/Search';
+import FilterIcon from '@material-ui/icons/Filter';
+import { useTranslation } from 'react-i18next';
 
 import PanelApp, {PanelProps} from '../appbar/panel';
 
 export default function HowtoPanel(props: PanelProps): JSX.Element {    
+    const { t } = useTranslation();
     
     return (
         <PanelApp
@@ -14,8 +18,15 @@ export default function HowtoPanel(props: PanelProps): JSX.Element {
             closeFunction = {props.closeFunction}
             content={
                 ((
-                    <Typography variant="body2" color="textSecondary" component="div">
-                        ///  How to contents here
+                    <Typography variant="body2" color="textSecondary" component="div">                        
+                        <h4 className="text-center"> {<SearchIcon/>} {t('appbar.search')} </h4>
+                        <p>  {t('howto.geosearchdescription')} </p>                        
+                        
+                        <h4 className="text-center">  {<KeywordSearchIcon/>} {t('appbar.keywordsearch')}  </h4>
+                        <p>  {t('howto.keywordsearchdescription')} </p>                        
+                        
+                        <h4 className="text-center">  {<FilterIcon/>} {t('appbar.filters')} </h4>
+                        <p>  {t('howto.filtersdescription')} </p>                              
                     </Typography>
                 ) as unknown) as Element
             }
