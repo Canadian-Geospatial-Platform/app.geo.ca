aa149c7556ad5897ee9a00c8082ce64b62736996
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2022-01-13 11:13:08 -0500

    form input is now alligned with valid typescript

--- src/components/creationform/creation-form.tsx
+++ src/components/creationform/creation-form.tsx
@@ -2,27 +2,31 @@ import React, { useState } from 'react';
 import './creation-form.css';
 
 function CreationForm() {
-  const [formData, setFormData] = useState({})
-   const handleChange = (event) => {
+  const [formData, setFormData] = useState({
+      fileidentifier: "",
+      age: 0
+  });
+   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
     const value = event.target.value;
     setFormData(values => ({...values, [name]: value}))
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     alert(JSON.stringify(formData));
   }
  
   return (
-    <div className="creation-form form-group">
+    <div className="creation-form">
     <form onSubmit={handleSubmit}>
+    <h2>Indentification</h2>
     <div className="form-group">
-      <label>Enter your name:
+      <label>File Identifier:
       <input 
         type="text" 
-        name="username" 
-        value={formData.username || ""} 
+        name="fileidentifier" 
+        value={formData.fileidentifier || ""} 
         onChange={handleChange}
       />
       </label>
