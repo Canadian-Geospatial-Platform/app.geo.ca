04517d6cef9c02cff50493b046f168065f34d180
chris <chris@indiciumx.com>
2022-05-31 23:16:10 -0400

    2205312316

--- src/common/queryparams.ts
+++ src/common/queryparams.ts
@@ -6,12 +6,12 @@ export function getQueryParams(querystring: string):{ [key: string]: string } {
         querystring
             .substr(1)
             .split('&')
+            .replace(/\+/g, ' ')
             .forEach((q: string) => {
                 const item = q.split('=');
                 /* const item = item_split.replace(/\+/g, ' ');*/
                 queryParams[item[0]] = decodeURI(item[1]);
             });
-        console.log(querystring);
     }
     return queryParams;
 }
