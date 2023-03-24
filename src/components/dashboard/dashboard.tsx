85cd83f75a9c9662c7bf4accaa9d624d4cb680ce
Raviinder Singh <raviinder@gmail.com>
2023-03-24 20:32:03 +0530

    Point 6,7 and 8 is closed

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -218,6 +218,90 @@ const Dashboard = () => {
             </section>
             <section className="sec-analytics-results sec-analytics-results-summary">
                 <div className="sec-inner-wrap">
+                    <div className="sec-inner-wrap no-margin no-border">
+                        <div className="card-wrap">
+                            <div className="width-50-per">
+                                <div className="sec-inner-wrap no-margin" >
+                                    <div className="card-wrap">
+                                        <h4 className="summary-card-title">
+                                            {t('dashboard.savedSearches')}
+                                        </h4>
+                                    </div>
+                                    <div className="card-wrap">
+                                        {savedSearchesLoading ? (
+                                            <BeatLoader color="#ffffff" />
+                                        ) : !savedSearches ? (
+                                            <div className="loading-failed">
+                                                {t('dashboard.loadingfailed')},{' '}
+                                                <button type="button" className="link-button" onClick={() => setSavedSearchesLoading(true)}>
+                                                    {t('analytic.tryagain')}
+                                                </button>
+                                            </div>
+                                        ) : (
+                                            <div >
+                                                <table className="anayltics-table table caption-top">
+                                                    <tbody>
+                                                        {
+                                                            savedSearches.map(q => {
+                                                                return <tr key={q.key} className="table-row table-row-no-record" style={{ textAlign: 'left' }}>
+                                                                    <td className="table-cell">{q.search}</td>
+                                                                    <td className="table-cell" style={{ cursor: 'pointer' }} onClick={() => { deleteSavedSearchItem(q.key, DASHBOARD_CALLS.SAVED_SEARCHES) }} >{<DeleteOutlined />}</td>
+                                                                </tr>
+                                                                //return <li > {q.search} </li>
+                                                            })}
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="width-50-per">
+                                <div className="sec-inner-wrap no-margin" >
+                                    <div className="card-wrap">
+                                        <h4 className="summary-card-title">
+                                            {t('dashboard.savedRecords')}
+                                        </h4>
+                                    </div>
+                                    <div className="card-wrap">
+                                        {savedRecordsLoading ? (
+                                            <BeatLoader color="#ffffff" />
+                                        ) : !savedRecords ? (
+                                            <div className="loading-failed">
+                                                {t('dashboard.loadingfailed')},{' '}
+                                                <button type="button" className="link-button" onClick={() => setsavedRecordsLoading(true)}>
+                                                    {t('analytic.tryagain')}
+                                                </button>
+                                            </div>
+                                        ) : (
+                                            <div >
+                                                <table className="anayltics-table table caption-top">
+                                                    <tbody>
+                                                        {
+                                                            savedRecords.map(q => {
+                                                                return <tr key={q.key} className="table-row table-row-no-record" style={{ textAlign: 'left' }}>
+                                                                    <td className="table-cell">{q["title_" + language]}</td>
+                                                                    <td className="table-cell" style={{ cursor: 'pointer' }} onClick={() => { deleteSavedSearchItem(q.key, DASHBOARD_CALLS.SAVED_RECORDS) }} >{<DeleteOutlined />}</td>
+                                                                </tr>
+                                                                //return <li > {q.search} </li>
+                                                            })}
+                                                    </tbody>
+                                                </table>
+                                            </div>
+
+                                        )}
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="sec-analytics-results sec-analytics-results-summary">
+                <div className="sec-inner-wrap">
                     <h2 className="sec-title no-margin"> {t('dashboard.latestCommunityAnnouncement')}
                     </h2>
                     <div className="sec-inner-wrap no-margin" >
@@ -328,90 +412,6 @@ const Dashboard = () => {
                             </div>
                             <div className="width-50-per text-right">
                                 <a href='#' className='color-light'>Go to Resource</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="sec-analytics-results sec-analytics-results-summary">
-                <div className="sec-inner-wrap">
-                    <div className="sec-inner-wrap no-margin no-border">
-                        <div className="card-wrap">
-                            <div className="width-50-per">
-                                <div className="sec-inner-wrap no-margin" >
-                                    <div className="card-wrap">
-                                        <h4 className="summary-card-title">
-                                            {t('dashboard.savedSearches')}
-                                        </h4>
-                                    </div>
-                                    <div className="card-wrap">
-                                        {savedSearchesLoading ? (
-                                            <BeatLoader color="#ffffff" />
-                                        ) : !savedSearches ? (
-                                            <div className="loading-failed">
-                                                {t('dashboard.loadingfailed')},{' '}
-                                                <button type="button" className="link-button" onClick={() => setSavedSearchesLoading(true)}>
-                                                    {t('analytic.tryagain')}
-                                                </button>
-                                            </div>
-                                        ) : (
-                                            <div >
-                                                <table className="anayltics-table table caption-top">
-                                                    <tbody>
-                                                        {
-                                                            savedSearches.map(q => {
-                                                                return <tr key={q.key} className="table-row table-row-no-record" style={{ textAlign: 'left' }}>
-                                                                    <td className="table-cell">{q.search}</td>
-                                                                    <td className="table-cell" style={{ cursor: 'pointer' }} onClick={() => { deleteSavedSearchItem(q.key, DASHBOARD_CALLS.SAVED_SEARCHES) }} >{<DeleteOutlined />}</td>
-                                                                </tr>
-                                                                //return <li > {q.search} </li>
-                                                            })}
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="width-50-per">
-                                <div className="sec-inner-wrap no-margin" >
-                                    <div className="card-wrap">
-                                        <h4 className="summary-card-title">
-                                            {t('dashboard.savedRecords')}
-                                        </h4>
-                                    </div>
-                                    <div className="card-wrap">
-                                        {savedRecordsLoading ? (
-                                            <BeatLoader color="#ffffff" />
-                                        ) : !savedRecords ? (
-                                            <div className="loading-failed">
-                                                {t('dashboard.loadingfailed')},{' '}
-                                                <button type="button" className="link-button" onClick={() => setsavedRecordsLoading(true)}>
-                                                    {t('analytic.tryagain')}
-                                                </button>
-                                            </div>
-                                        ) : (
-                                            <div >
-                                                <table className="anayltics-table table caption-top">
-                                                    <tbody>
-                                                        {
-                                                            savedRecords.map(q => {
-                                                                return <tr key={q.key} className="table-row table-row-no-record" style={{ textAlign: 'left' }}>
-                                                                    <td className="table-cell">{q["title_" + language]}</td>
-                                                                    <td className="table-cell" style={{ cursor: 'pointer' }} onClick={() => { deleteSavedSearchItem(q.key, DASHBOARD_CALLS.SAVED_RECORDS) }} >{<DeleteOutlined />}</td>
-                                                                </tr>
-                                                                //return <li > {q.search} </li>
-                                                            })}
-                                                    </tbody>
-                                                </table>
-                                            </div>
-
-                                        )}
-
-
-
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
