dd7ab94fea1cf0eee1bac5220ea12bb78830c14c
Pascal Vautour <vautour.pascal@gmail.com>
2023-05-16 13:47:00 -0400

    hardcoded redirect functions.

--- src/components/signin/signin.tsx
+++ src/components/signin/signin.tsx
@@ -1,5 +1,11 @@
 import './signin.scss'
+import React, { useEffect } from "react";
 
 export default function SignIn(): JSX.Element {
-    return <h1 className="sign-in">Welcome Message!</h1>;
+     useEffect(() => {
+    window.location.href = "https://auth-dev.geo.ca/oauth2/authorize?client_id=7b4mbo0osnfb6cer4f980kob0t&response_type=code&scope=openid&redirect_uri=http://localhost:8080";
+  }, []);
+
+  
+    return <h1 className="sign-in">Please wait while you are being redirected to the sign-in page.</h1>;
 }
