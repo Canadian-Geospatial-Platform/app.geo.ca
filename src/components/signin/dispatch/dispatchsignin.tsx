0f9299f36cabae3fcae4d7d77ce1dbc00b752b96
Pascal Vautour <vautour.pascal@gmail.com>
2023-05-17 10:01:35 -0400

    we now go through sign in flow and redirect to receive sign in page.

--- src/components/signin/dispatch/dispatchsignin.tsx
+++ src/components/signin/dispatch/dispatchsignin.tsx
@@ -1,11 +1,32 @@
 import './dispatchsignin.scss'
 import React, { useEffect } from "react";
+import { requireLogin } from '../utils/authorization';
+
+function getToken() {
+  try {
+    const storedToken = sessionStorage.getItem('token')
+    if (storedToken) {
+      const token = JSON.parse(storedToken);
+      return token;
+    }
+    return null
+  } catch (error) {
+    return null;
+  }
+}
 
 export default function DispatchSignIn(): JSX.Element {
-     useEffect(() => {
-    window.location.href = "https://auth-dev.geo.ca/oauth2/authorize?client_id=7b4mbo0osnfb6cer4f980kob0t&response_type=code&scope=openid&redirect_uri=http://localhost:8080/receivesignin";
-  }, []);
+  useEffect(() => {
+    console.log("init");
+    // window.location.href = "https://auth-dev.geo.ca/oauth2/authorize?client_id=7b4mbo0osnfb6cer4f980kob0t&response_type=code&scope=openid&redirect_uri=http://localhost:8080/receivesignin";
+    const fetchData = async () => {
+      console.log("init2" + window.location.href);
 
-  
-    return <h1 className="sign-in">Please wait while you are being redirected to the sign-in page.</h1>;
+      await requireLogin(getToken(), window.location.origin + '/receivesignin', window.location.href);
+    }
+    fetchData()
+  }, [requireLogin]);
+
+
+  return <h1 className="sign-in">Please wait while you are being redirected to the sign-in page.</h1>;
 }
--- src/components/signin/receive/receivesignin.tsx
+++ src/components/signin/receive/receivesignin.tsx
@@ -1,4 +1,9 @@
+import './receivesignin.scss'
+import React, { useEffect } from "react";
 export default function ReceiveSignIn(): JSX.Element {
+       useEffect(() => {
+    // window.location.href = "https://auth-dev.geo.ca/oauth2/authorize?client_id=7b4mbo0osnfb6cer4f980kob0t&response_type=code&scope=openid&redirect_uri=http://localhost:8080/receivesignin";
+  }, []);
   
-    return <h1 className="receive">Receiving sign in.</h1>;
+    return <h1 className="receivesignin">Receiving sign in.</h1>;
 }
