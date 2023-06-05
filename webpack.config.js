ab48abd9531316dd3b31eabaf4f9aa4edcca4d25
johnweng001 <johnzweng@gmail.com>
2023-06-05 13:38:41 -0400

    remove ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
       },
       {
         test: /\.(css|scss)$/,
