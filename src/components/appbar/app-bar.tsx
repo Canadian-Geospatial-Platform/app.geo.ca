ba1acbd4bca9b4c3d1d92c1bb5171ce454616242
johnweng001 <johnzweng@gmail.com>
2023-06-07 09:58:11 -0400

    added store context

--- src/components/appbar/app-bar.tsx
+++ src/components/appbar/app-bar.tsx
@@ -1,6 +1,32 @@
-import { useDispatch } from "react-redux";
+import { useContext } from "react";
+
+import { AppContext } from "../..";
+import { useSelector } from "react-redux";
+import { decrement, increment } from "../../features/counter";
 
 export function Appbar(): JSX.Element {
-  const dispatch = useDispatch();
-  return <div> This is appbar</div>;
+  const appContext = useContext(AppContext);
+  const dispatch = appContext.store.dispatch;
+  //const count = appContext.store.getState().counter.value;
+  const count = useSelector((state: any) => state.counter.value);
+  return (
+    <div>
+      This is appbar
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import { App } from "./app";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { createContext } from "react";
+export const AppContext = createContext({ store });
 ReactDOM.render(
   <Provider store={store}>
     <App />
