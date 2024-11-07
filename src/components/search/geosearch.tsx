7cafd78cbd8f33d85060c6358942e83e87f1f084
Bo Lu <bo.lu@canada.ca>
2024-11-07 09:23:58 -0500

    Update geosearch.tsx

--- src/components/search/geosearch.tsx
+++ src/components/search/geosearch.tsx
@@ -261,7 +261,9 @@ const GeoSearch = (
                     // GEO.ca record
                     //const zoomLevel = map.getZoom();
                     // Calculate a more appropriate longitude offset for Canada (scale it based on zoom level)
-
+                    const zoomLevel = map.getBoundsZoom(bounds);
+                    const longitudeOffset = 100 * (zoomLevel / 10); // Adjust this formula for better control
+                    const adjustedCenter = new LatLng(center.lat, center.lng + longitudeOffset);
                     // Adjust the center after the map is set
                     console.log("ELSE1")
                     console.log(center.lat)
