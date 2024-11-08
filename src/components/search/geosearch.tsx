ac198251d061bfb4ec864792508dd4b94e91a223
Bo Lu <bo.lu@canada.ca>
2024-11-08 10:11:22 -0500

    Update geosearch.tsx

--- src/components/search/geosearch.tsx
+++ src/components/search/geosearch.tsx
@@ -255,7 +255,7 @@ const GeoSearch = (
                     map.fitBounds(L.geoJSON(data).getBounds(), { paddingTopLeft: [padding[0], padding[1]], paddingBottomRight: [50, 50] });
                     setTimeout(() => {
                         new L.geoJSON(data).addTo(map);
-                        map.panBy([0, 300]);
+                        map.panBy([-300, 0]);
                     }, 200);
                 }
             } else{
